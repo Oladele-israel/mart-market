8f8e1f92551017e78b9b5e9439e7a4622a46ac02
Oladele-israel <omoleoladele43@gmail.com>
2024-04-28 15:03:20 +0100

    updated cors

--- mart backend/index.js
+++ mart backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://mart-market.vercel.app/", "*", "http://localhost:5173"],
+    origin: ["https://mart-market.vercel.app/", "http://localhost:5173"],
     methods: "GET, POST",
     credentials: true,
   })
